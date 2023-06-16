b79cc645520aa506f426e298a26f9d0a96b7067a
CindyWasCoding <134302235+CindyWasCoding@users.noreply.github.com>
2023-06-15 18:45:59 -0700

    Update FootballScore.java

--- Lecture3and4Q7/FootballScore.java
+++ Lecture3and4Q7/FootballScore.java
@@ -1,21 +1,25 @@
-package Lecture3and4Q7;
-
 /* Start with the FootballScore class code from the lecture.   Write a reasonable .equals() method for this class.*/
+
+package Lecture3and4Q7;
 public class FootballScore {
 	private int score;
 	
 	public int getScore() {
 		return score;
 	}
+	
 	public void touchdown() {
 		score += 6;
 	}
+	
 	public void extraPoint() {
 		score += 1;
 	}
+	
 	public void safety() {
 		score += 2;
 	}
+	
 	public void fieldGoal() {
 		score += 3;
 	}

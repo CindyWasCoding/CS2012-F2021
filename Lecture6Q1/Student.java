a278998e704c11749f8b663430fd2707562dcff9
CindyWasCoding <134302235+CindyWasCoding@users.noreply.github.com>
2023-06-15 18:47:13 -0700

    Update Student.java

--- Lecture6Q1/Student.java
+++ Lecture6Q1/Student.java
@@ -3,12 +3,10 @@ package Lecture6Q1;
 public class Student extends Person{
 	double gpa;
 	
-	
 	public Student(String name, double gpa) {
 		this.name = name;
 		this.gpa = gpa;
 	}
-	
 	
 	public void setName(String name) {
 		this.name = name;
@@ -17,11 +15,9 @@ public class Student extends Person{
 		this.gpa = gpa;
 	}
 	
-	
 	public String toString() {
 		return "Student named " + this.name + " has a GPA of " + this.gpa;
 	}
-	
 	
 	public double getGpa() {
 		return this.gpa;
